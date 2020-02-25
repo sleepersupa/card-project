c2bf713017c7273f196496f5988ee2efbddb432d
Huy <buiduchuy25@gmail.com>
2020-02-25 16:59:59 +0700

    add games

--- build/server.js
+++ build/server.js
@@ -4,44 +4,18 @@ var compression = require('compression')
 app.use(compression())
 const path = require("path");
 const bodyParser = require("body-parser");
-const multer = require("multer");
-const storage= multer.diskStorage({
-    destination:(req, file, cb)=>{
-        cb(null,__dirname+ "/uploads/") ;
-    },
-    filename:(req, file, cb)=>{
-        // cb(null,file.originalname) ;
-        // console.log(file)
-        let tail = file.originalname.split(".")[1];
-        console.log(tail)
-        let ran = Math.ceil(Math.random() *1000 +1) ;
-        cb(null,  ran +file.originalname) ;
-    }
-})
-const upload= multer({storage : storage})
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/shop_ao_dai", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/card-project", { useNewUrlParser: true });
 app.use(express.static(__dirname) );
 app.use("/api", bodyParser.json());
 let router = express.Router();
 app.use("/api", router);
-
-app.post("/api/file/upload" , upload.single('imageFile') , (req,res) =>{
-    // console.log(req.file)
-    if(req.file){
-        // setTimeout(()=>{
-            res.send({filePath: "/uploads/"+req.file.filename})
-        // },5000)
-    }
-})
 require("../server-controllers/user-controller")(router)
-require("../server-controllers/product-controller")(router)
+require("../server-controllers/upload-controller")(router)
+require("../server-controllers/card-controller")(router)
 app.get("*",(req, res, next) => {
     res.sendFile(__dirname + "/index.html");
 });
-
-
-
 var server = app.listen(process.env.PORT || 5001, function () {
     var port = server.address().port;
 
